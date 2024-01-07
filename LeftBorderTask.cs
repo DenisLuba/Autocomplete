cb9cb6b902974a2b5aa2606a814044807b9cbc17
DenisLuba <denislubapershins@gmail.com>
2024-01-07 23:36:03 +0300

    Implemented the method GetLeftBorderIndex in the class LeftBorderTask

--- LeftBorderTask.cs
+++ LeftBorderTask.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Autocomplete;
 
@@ -11,25 +10,32 @@ namespace Autocomplete;
 // Во всех функциях сравнения строк в C# можно передать способ сравнения.
 public class LeftBorderTask
 {
-	/// <returns>
-	/// Возвращает индекс левой границы.
-	/// То есть индекс максимальной фразы, которая не начинается с prefix и меньшая prefix.
-	/// Если такой нет, то возвращает -1
-	/// </returns>
-	/// <remarks>
-	/// Функция должна быть рекурсивной
-	/// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
-	/// </remarks>
-	public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
-	{
-		// IReadOnlyList похож на List, но у него нет методов модификации списка.
-		// Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
-		for (int i = 0; i < phrases.Count; i++)
-		{
-			if (string.Compare(prefix, phrases[i], StringComparison.InvariantCultureIgnoreCase) < 0
-			    || phrases[i].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
-				return i - 1;
-		}
-		return phrases.Count-1;
-	}
+    /// <returns>
+    /// Возвращает индекс левой границы.
+    /// То есть индекс максимальной фразы, которая не начинается с prefix и меньшая prefix.
+    /// Если такой нет, то возвращает -1
+    /// </returns>
+    /// <remarks>
+    /// Функция рекурсивная и работает за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
+    /// </remarks>
+
+    public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+    {
+        if (phrases.Count == 0) return -1;
+        left = left <= 0 || left >= phrases.Count ? 0 : left;
+        right = right <= 0 || right >= phrases.Count ? phrases.Count - 1 : right;
+        return BinarySearchLeftBorder(phrases, prefix, left, right);
+    }
+
+    public static int BinarySearchLeftBorder(IReadOnlyList<string> phrases, string prefix, int left, int right)
+    {
+        if (left >= right)
+            return string.Compare(phrases[left], prefix, StringComparison.InvariantCultureIgnoreCase) < 0
+                ? left
+                : left - 1;
+        var middle = left + (right - left) / 2;
+        return string.Compare(phrases[middle], prefix, StringComparison.InvariantCultureIgnoreCase) < 0
+            ? BinarySearchLeftBorder(phrases, prefix, middle + 1, right)
+            : BinarySearchLeftBorder(phrases, prefix, left, middle - 1);
+    }
 }
