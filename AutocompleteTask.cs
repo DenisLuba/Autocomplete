e39ad7839f3135ca7703631ebcb31b69a28f3dfa
DenisLuba <denislubapershins@gmail.com>
2024-01-08 01:18:32 +0300

    Implemented the method GetRightBorderIndex in the class RightBorderTask

--- AutocompleteTask.cs
+++ AutocompleteTask.cs
@@ -7,61 +7,80 @@ namespace Autocomplete;
 
 internal class AutocompleteTask
 {
-	/// <returns>
-	/// Возвращает первую фразу словаря, начинающуюся с prefix.
-	/// </returns>
-	/// <remarks>
-	/// Эта функция уже реализована, она заработает, 
-	/// как только вы выполните задачу в файле LeftBorderTask
-	/// </remarks>
-	public static string FindFirstByPrefix(IReadOnlyList<string> phrases, string prefix)
-	{
-		var index = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1;
-		if (index < phrases.Count && phrases[index].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
-			return phrases[index];
-            
-		return null;
-	}
+    /// <returns>
+    /// Возвращает первую фразу словаря, начинающуюся с prefix.
+    /// </returns>
+    /// <remarks>
+    /// Эта функция уже реализована, она заработает, 
+    /// как только вы выполните задачу в файле LeftBorderTask
+    /// </remarks>
+    public static string FindFirstByPrefix(IReadOnlyList<string> phrases, string prefix)
+    {
+        var index = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1;
+        if (index < phrases.Count && phrases[index].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
+            return phrases[index];
 
-	/// <returns>
-	/// Возвращает первые в лексикографическом порядке count (или меньше, если их меньше count) 
-	/// элементов словаря, начинающихся с prefix.
-	/// </returns>
-	/// <remarks>Эта функция должна работать за O(log(n) + count)</remarks>
-	public static string[] GetTopByPrefix(IReadOnlyList<string> phrases, string prefix, int count)
-	{
-		// тут стоит использовать написанный ранее класс LeftBorderTask
-		return null;
-	}
+        return null;
+    }
 
-	/// <returns>
-	/// Возвращает количество фраз, начинающихся с заданного префикса
-	/// </returns>
-	public static int GetCountByPrefix(IReadOnlyList<string> phrases, string prefix)
-	{
-		// тут стоит использовать написанные ранее классы LeftBorderTask и RightBorderTask
-		return -1;
-	}
+    /// <returns>
+    /// Возвращает первые в лексикографическом порядке count (или меньше, если их меньше count) 
+    /// элементов словаря, начинающихся с prefix.
+    /// </returns>
+    /// <remarks>Эта функция должна работать за O(log(n) + count)</remarks>
+    public static string[] GetTopByPrefix(IReadOnlyList<string> phrases, string prefix, int count)
+    {
+        // тут стоит использовать написанный ранее класс LeftBorderTask
+        return null;
+    }
+
+    /// <returns>
+    /// Возвращает количество фраз, начинающихся с заданного префикса
+    /// </returns>
+    public static int GetCountByPrefix(IReadOnlyList<string> phrases, string prefix)
+    {
+        // тут стоит использовать написанные ранее классы LeftBorderTask и RightBorderTask
+        return -1;
+    }
 }
 
 [TestFixture]
 public class AutocompleteTests
 {
-	[Test]
-	public void TopByPrefix_IsEmpty_WhenNoPhrases()
-	{
-		// ...
-		//CollectionAssert.IsEmpty(actualTopWords);
-	}
+    [Test]
+    public void TopByPrefix_IsEmpty_WhenNoPhrases()
+    {
+        // ...
+        //CollectionAssert.IsEmpty(actualTopWords);
+    }
 
-	// ...
+    // ...
 
-	[Test]
-	public void CountByPrefix_IsTotalCount_WhenEmptyPrefix()
-	{
-		// ...
-		//Assert.AreEqual(expectedCount, actualCount);
-	}
+    [Test]
+    public void CountByPrefix_IsTotalCount_WhenEmptyPrefix()
+    {
+        // ...
+        //Assert.AreEqual(expectedCount, actualCount);
+    }
 
-	// ...
+    // ...
+
+
+    [TestCase(new[] { "a", "a" }, "a", 2, 1)]
+    [TestCase(new[] { "a", "b" }, "a", 1, 2)]
+    [TestCase(new[] { "b", "b" }, "a", 0, 3)]
+    [TestCase(new[] { "a", "b", "b", "b" }, "a", 1, 4)]
+    [TestCase(new[] { "ab", "b", "b", "b" }, "a", 1, 5)]
+    [TestCase(new[] { "ab", "b", "b", "b" }, "ab", 1, 6)]
+    [TestCase(new string[0] { }, "a", 0, 7)]
+    [TestCase(new string[] { "a", "ab", "abc" }, "aa", 1, 8)]
+    [TestCase(new string[] { "ab", "ab", "ab", "ab" }, "a", 4, 9)]
+    [TestCase(new string[] { "ab", "ab", "ab", "ab" }, "aa", 0, 10)]
+
+    public void TestCases(string[] arr, string prefix, int expectedResult, int testNumber)
+    {
+        var phrases = arr.ToList();
+        Console.WriteLine($"Test: {testNumber} Expected: {expectedResult}");
+        Assert.AreEqual(expectedResult, RightBorderTask.GetRightBorderIndex(phrases, prefix, -1, phrases.Count()));
+    }
 }
--- RightBorderTask.cs
+++ RightBorderTask.cs
@@ -1,30 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Autocomplete;
 
 public class RightBorderTask
 {
-	/// <returns>
-	/// Возвращает индекс правой границы. 
-	/// То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
-	/// Если такого нет, то возвращает items.Length
-	/// </returns>
-	/// <remarks>
-	/// Функция должна быть НЕ рекурсивной
-	/// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
-	/// </remarks>
-	public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
-	{
-		// IReadOnlyList похож на List, но у него нет методов модификации списка.
-		// Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
-		for (int i = phrases.Count-1; i >= 0; i--)
-		{
-			if (string.Compare(prefix, phrases[i], StringComparison.InvariantCultureIgnoreCase) >= 0 
-			    || phrases[i].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
-				return i + 1;
-		}
-		return 0;
-	}
+    /// <returns>
+    /// Возвращает индекс правой границы. 
+    /// То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
+    /// Если такого нет, то возвращает items.Length
+    /// </returns>
+    /// <remarks>
+    /// Функция НЕ рекурсивная и работает за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
+    /// </remarks>
+
+    public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+    {
+        if (phrases.Count == 0) return 0;
+        left = left <= 0 || left >= phrases.Count ? 0 : left;
+        right = right <= 0 || right >= phrases.Count ? phrases.Count - 1 : right;
+        while (left < right)
+        {
+            var middle = left + (right - left) / 2;
+            if (string.Compare(phrases[middle], prefix, StringComparison.InvariantCultureIgnoreCase) <= 0
+                || phrases[middle].Contains(prefix, StringComparison.InvariantCultureIgnoreCase))
+                left = middle + 1;
+            else right = middle - 1;
+        }
+
+        right = right < 0 ? 0 : right;
+        return string.Compare(prefix, phrases[right], StringComparison.InvariantCultureIgnoreCase) >= 0
+               || phrases[right].Contains(prefix, StringComparison.InvariantCultureIgnoreCase)
+            ? right + 1
+            : right;
+    }
 }
